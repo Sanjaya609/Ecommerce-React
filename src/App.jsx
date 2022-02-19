93efab17a9202e0778c80d0df20cb86d635983c4
Sanjaya609 <Sanjayakhatri5@gmail.com>
2022-02-19 21:41:30 +0545

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Ecommerce } from './Ecommerce'
+
+function App() {
+  return (
+    <Ecommerce/>
+  )
+}
+
+export default App
